2e6d21ef10523d09fc3867da035eaedc49faeed7
kanty05 <kamrunnaharkantyy@gmail.com>
2025-08-02 11:35:53 +0600

    second commit

---
+++ src/main/java/mainpckg/kfc1/KitchenEmployee/KitchenShiftLogController.java
@@ -0,0 +1,23 @@
+package mainpckg.kfc1.KitchenEmployee;
+
+public class KitchenShiftLogController
+{
+    @javafx.fxml.FXML
+    private Label endShiftTimeLable;
+    @javafx.fxml.FXML
+    private Label startShiftTimeLable;
+    @javafx.fxml.FXML
+    private Label durationLable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void endShiftButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void startShiftButtonOnAction(ActionEvent actionEvent) {
+    }
+}
