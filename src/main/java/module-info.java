fcae6399bf781b51958fdf0f51c61e485928e3ad
kanty05 <kamrunnaharkantyy@gmail.com>
2025-08-02 08:46:46 +0600

    first commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module mainpckg.kfc1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
 
     opens mainpckg.kfc1 to javafx.fxml;
+
+    opens mainpckg.kfc1.Customer to javafx.fxml, java.base ;
+    opens mainpckg.kfc1.KitchenEmployee to javafx.fxml, java.base ;
+
     exports mainpckg.kfc1;
 }
