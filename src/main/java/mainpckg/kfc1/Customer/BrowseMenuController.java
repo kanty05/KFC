8057b04b636ec89c0f9e90a0259457f078218e03
kanty05 <kamrunnaharkantyy@gmail.com>
2025-08-16 12:49:45 +0600

    second commit

--- src/main/java/mainpckg/kfc1/Customer/BrowseMenuController.java
+++ src/main/java/mainpckg/kfc1/Customer/BrowseMenuController.java
@@ -14,8 +14,7 @@ import mainpckg.kfc1.HelloApplication;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class BrowseMenuController
-{
+public class BrowseMenuController {
 
     @FXML
     private TabPane manuTabPane;
@@ -54,48 +53,45 @@ public class BrowseMenuController
 
     @FXML
     public void twelvePcChickenBucketAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("12Pc Chicken Bucket",1799,twelvePcChickenQuantityTextField);
     }
 
 
     @FXML
     public void eightPcChickenBucketAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("8Pc Chicken Bucket",1279,eightPcChickenQuantityTextField);
     }
 
     @FXML
     public void friesLargeAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Fries-Large",299,friesLargeQuantityTextField);
     }
 
     @FXML
     public void friesMediumAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Fries-Medium",199,friesMediumQuantityTextField);
+
     }
 
     @FXML
     public void friesSmallAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Fries-Small",149,friesSmallQuantityTextField);
+
     }
 
     @FXML
     public void zingerBurgerAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Zinger Burger",299,zingerBurgerQuantityTextField);
+
     }
 
     @FXML
     public void superChargerBurgerAddtoCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Super Charger Burger",479,superChargerBurgerQuantityTextField);
+
     }
 
     @FXML
     public void doubleZingerBurgerAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("Double Charger Burger",599,doubleZingerBurgerQuantityTextField);
+
     }
 
     @FXML
     public void sixPcWingsBucketAddToCartButtonOnAction(ActionEvent actionEvent) {
-        addToCart("6Pc Wings Bucket",329,sixPcChickenQuantityTextField);
+
     }
 
     private void addToCart(String itemName,double price,TextField quantity){
--- src/main/java/mainpckg/kfc1/Customer/CartController.java
+++ src/main/java/mainpckg/kfc1/Customer/CartController.java
@@ -1,20 +1,11 @@
 package mainpckg.kfc1.Customer;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import mainpckg.kfc1.HelloApplication;
-import mainpckg.kfc1.KitchenEmployee.KitchenEmployeeDashboardController;
 
 import java.io.IOException;
 
@@ -30,22 +21,37 @@ public class CartController {
     @javafx.fxml.FXML
     private VBox cartVbox;
     @javafx.fxml.FXML
-    private TableView<Customer> cartTableView;
+    private TableView<Cart> cartTableView;
     @javafx.fxml.FXML
     private Button checkOutButton;
     @javafx.fxml.FXML
     private Label labelmsg;
 
 
+    ObservableList<Cart> carts = FXCollections.observableArrayList();
+
     @javafx.fxml.FXML
     public void initialize() {
-        itemNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("items"));
+        itemNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("foodItem"));
         quantityTableColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
         priceTableColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        carts.add(new Cart("Fries-Small",2,358));
+        carts.add(new Cart("zinger Burger",1,299));
+        carts.add(new Cart("8Pc hot and crispy Chicken Bucket",1,1299));
+        carts.add(new Cart("Super Charger Burger",1,479));
+
+        totalPriceLable.setText("Total: Tk.2435");
+
+        cartTableView.setItems(carts);
 
     }
 
     @javafx.fxml.FXML
     public void checkOutButtonOnAction(ActionEvent actionEvent) throws IOException {
+        Alert alert=new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Go to Place Order to Confirm your order by giving delivery detail!");
+        alert.showAndWait();
     }
+
 }
